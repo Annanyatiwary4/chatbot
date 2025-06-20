f0b40e9a3310d89ed43668f652466d8c7aa4bba8
Annanya Tiwary <annanyatiwary4@gmail.com>
2025-06-21 01:44:28 +0530

    *

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 set -o errexit
 
 pip install -r requirements.txt
+
+# ✅ Download NLTK resources
+python -m nltk.downloader punkt
 python manage.py collectstatic --no-input
 python manage.py migrate
